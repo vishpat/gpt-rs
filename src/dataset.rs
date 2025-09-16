e6180dfba627cb3bd2f01a1903c6a997f44ab3b0
Vishal Patil <vishpat@gmail.com>
2025-09-16 13:19:11 -0400

    Checkpoint

---
+++ src/dataset.rs
@@ -0,0 +1,50 @@
+use candle_core::Tensor;
+use anyhow::Result;
+use std::fs::File;
+use std::io::Read;
+
+use crate::vocab::Vocab;
+use crate::BLOCK_SIZE;
+use crate::BATCH_SIZE;
+use std::rc::Rc;
+use candle_core::Device;
+use rand::Rng;
+
+fn read_data(data_file: &str, vocab: &Vocab) -> Result<Tensor> {
+    let mut file = File::open(data_file)?;
+    let mut data = String::new();
+    file.read_to_string(&mut data)?;
+    vocab.encode(&data)
+}
+
+pub enum DatasetType {
+    Train,
+    Test,
+}
+
+pub struct Dataset {
+    device: Rc<Device>,
+    train_data: Tensor,
+    test_data: Tensor,
+}
+
+impl Dataset {
+    pub fn new(data_file: &str, vocab: &Vocab, device: &Rc<Device>) -> Result<Self> {
+        let data = read_data(data_file, vocab)?;
+        let size = data.dim(0)?;
+        let train_size = ((size as f32 * 0.9) as usize / BLOCK_SIZE) * BLOCK_SIZE;
+        let test_size = ((size - train_size) / BLOCK_SIZE) * BLOCK_SIZE;
+        let train_data = data.narrow(0, 0, train_size)?;
+        let test_data = data.narrow(0, train_size, test_size)?;
+        Ok(Self { device: device.clone(), train_data, test_data })
+    }
+
+    pub fn get_batch(&self, dataset_type: DatasetType) -> Result<(Tensor, Tensor)> {
+        let data = match dataset_type {
+            DatasetType::Train => &self.train_data,
+            DatasetType::Test => &self.test_data,
+        };
+        let random_index = rand::thread_rng().gen_range(0..data.dim(0)? - BATCH_SIZE);
+        Ok((data.narrow(0, random_index, BATCH_SIZE)?, data.narrow(0, random_index + BATCH_SIZE, BATCH_SIZE)?))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use candle_core::Device;
 use std::rc::Rc;
 
 use vocab::Vocab;
-use dataset::Dataset;
+use dataset::{Dataset, DatasetType};
 
 const BLOCK_SIZE: usize = 8;
 const BATCH_SIZE: usize = 4;
@@ -14,9 +14,10 @@ const BATCH_SIZE: usize = 4;
 fn main() -> Result<()> {
     let device = Rc::new(Device::cuda_if_available(0)?);
     let vocab = Vocab::new("input.txt", &device)?;
-    let dataset = Dataset::new("input.txt", &vocab)?;
-    println!("Train data: {:?}", dataset.train_data().shape());
-    println!("Test data: {:?}", dataset.test_data().shape());
+    let dataset = Dataset::new("input.txt", &vocab, &device)?;
+    let (x, y) = dataset.get_batch(DatasetType::Train)?;
+    println!("Train data: {:?}", x.shape());
+    println!("Test data: {:?}", y.shape());
 
     Ok(())
 }
